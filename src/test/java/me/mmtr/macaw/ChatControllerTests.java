655b77b8e31125cd82dfa6bc66148c202decb612
Velshh <pkot2003@gmail.com>
2025-03-08 19:28:16 +0100

    create unit tests for ChatController.java

---
+++ src/test/java/me/mmtr/macaw/ChatControllerTests.java
@@ -0,0 +1,42 @@
+package me.mmtr.macaw;
+
+import me.mmtr.macaw.controller.ChatController;
+import me.mmtr.macaw.data.ChatMessage;
+import me.mmtr.macaw.enums.MessageType;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.security.Principal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class ChatControllerTests {
+
+    @Mock
+    private Principal principal;
+
+    @InjectMocks
+    private ChatController chatController;
+
+    @Test
+    public void shouldThrowAnExceptionWhenRoomIdDoesNotContainUsersUsername() {
+        when(principal.getName()).thenReturn("unpermitted");
+
+        ChatMessage message = new ChatMessage("user1", "Message Content", MessageType.CHAT);
+
+
+        IllegalArgumentException thrownException = assertThrows(IllegalArgumentException.class, () ->
+                chatController.sendMessage(principal, "user1-user2", message));
+
+        assertEquals("You are not permitted to enter this room.", thrownException.getMessage());
+    }
+
+}
