950ce8f8c73652ae6aebf21c2f120df401e318bc
Velshh <pkot2003@gmail.com>
2025-03-09 10:39:22 +0100

    style login and registration page with Bootstrap

--- src/main/java/me/mmtr/macaw/configuration/WebSocketConfiguration.java
+++ src/main/java/me/mmtr/macaw/configuration/WebSocketConfiguration.java
@@ -11,8 +11,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws");
     }
 
     @Override
